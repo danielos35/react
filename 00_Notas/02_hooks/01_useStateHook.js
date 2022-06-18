397288d11c51737a4d362c0ffeed3e2e52be3cd0
Daniel <studiosdopplermusic@gmail.com>
2022-06-18 11:32:08 -0500

    hooks

--- 00_Notas/02_hooks/01_useStateHook.js
+++ 00_Notas/02_hooks/01_useStateHook.js
@@ -5,6 +5,11 @@ Documentacón:
 - https://es.reactjs.org/docs/hooks-intro.html
 
 
+ - Los hooks suelen llamarse con la preposición use por ejemplo useState. -
+
+
+USESTATE
+- El use state se suele presentar como un arreglo
 
 
 */
