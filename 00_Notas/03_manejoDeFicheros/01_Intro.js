e402eb905f32905351f29790fac0b74de73236c8
Daniel <studiosdopplermusic@gmail.com>
2022-06-19 21:10:33 -0500

    states

--- 00_Notas/03_manejoDeFicheros/01_Intro.js
+++ 00_Notas/03_manejoDeFicheros/01_Intro.js
@@ -1,9 +1,11 @@
 /*
 
-MANEJO DE ESTILOS
+MANEJO DE ESTILOS.
 - Al igual que en angular, si quiero tener unos estilos principales, los puedo colocar directamente en el componente padre.
-- 
 
+ESTRUCTURA DE CARPETAS.
+- Para estructurar carpetas podemos usar diferentes estrategias, entre ellas las recomendadas en la documentación.
+--https://es.reactjs.org/docs/faq-structure.html
 
 
 
--- 05_buscadorGif/src/buscador.jsx
+++ 05_buscadorGif/src/buscador.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
 export const Buscador = () => {
+  const [busquedas, setBusquedas] = useState(["Musica", "Politica"]);
+  console.log(busquedas);
   return (
     <>
       <h1>Buscador Gif</h1>
+
+      <ol>
+        {busquedas.map((c) => (
+          <li key={c}>{c}</li>
+        ))}
+      </ol>
     </>
   );
 };
