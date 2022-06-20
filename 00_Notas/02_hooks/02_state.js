16f2b8199f90ee17481510e89b05081b26387a8b
Daniel <studiosdopplermusic@gmail.com>
2022-06-20 18:33:35 -0500

    agregar estado

--- 00_Notas/02_hooks/02_state.js
+++ 00_Notas/02_hooks/02_state.js
@@ -7,4 +7,13 @@ ESTADO:
 - Esto se debe a que React siempre mantiene todos los datos del componente actualizados.
 - React está muy bien optimizado. 
 
+
+
+USESTATE
+
+- React trata de no hacer mutaciones y es por ello que que se debe de evitar el uso del push para trabjar con este hook. 
+- En estos casos se recomienda el uso de spread operator mas el dato a agregar [...datos, nuevos datos].
+- O usar el callBack que puede ser enviado a las funciones de setState =  setState(datos=>[...datos, nuevoDato]).
+- Ejemplo en 05_buscadorGif en buscador.jsx
+
 */
--- 05_buscadorGif/src/buscador.jsx
+++ 05_buscadorGif/src/buscador.jsx
@@ -2,11 +2,18 @@ import { useState } from "react";
 
 export const Buscador = () => {
   const [busquedas, setBusquedas] = useState(["Musica", "Politica"]);
-  console.log(busquedas);
+  const agregarCategorias = () => {
+
+    // Forma 1
+    // setBusquedas([...busquedas, "Hola Mundo"]);
+
+    // Forma2
+    setBusquedas(busquedas=>[...busquedas, 'Hola Mundo'])
+  };
   return (
     <>
       <h1>Buscador Gif</h1>
-
+      <button onClick={agregarCategorias}>Agregar categoria</button>
       <ol>
         {busquedas.map((c) => (
           <li key={c}>{c}</li>
