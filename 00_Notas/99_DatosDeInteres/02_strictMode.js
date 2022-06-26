32c64dd73c57421757e1034bc2b3d221f6d6f1c3
Daniel <studiosdopplermusic@gmail.com>
2022-06-25 19:25:40 -0500

    modo estricto

---
+++ 00_Notas/99_DatosDeInteres/02_strictMode.js
@@ -0,0 +1,11 @@
+/*
+STRICT MODE 
+
+- Permite identificar problemas en el momento de desarrollo del codigo.
+- Identificar componentes con ciclos de vida inseguros.
+- Documentación en español: https://runebook.dev/es/docs/react/strict-mode.
+- Documentación en ingles: https://reactjs.org/docs/strict-mode.html.
+-
+
+
+*/ 
--- 05_buscadorGif/src/funciones/getGifs.js
+++ 05_buscadorGif/src/funciones/getGifs.js
@@ -1,12 +1,12 @@
 
 export const getGifs = async (categoria)=>{
-        const url = `https://api.giphy.com/v1/gifs/search?api_key=BATA8D9T2GkMUePGXGwJQq1NGNFGdqJg&q=${categoria}&limit=20`; 
-        const respuesta = await fetch(url); 
-        const {data = []} = await respuesta.json()
-        const gifs = data.map( img => ({
-            id :img.id, 
-            title: img.title,
-            url: img.images.downsized_medium.url
-        }))
-        console.log(gifs);
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=BATA8D9T2GkMUePGXGwJQq1NGNFGdqJg&q=${categoria}&limit=20`; 
+    const respuesta = await fetch(url); 
+    const {data = []} = await respuesta.json()
+    const gifs = data.map( img => ({
+        id :img.id, 
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }))
+    return gifs
 }
