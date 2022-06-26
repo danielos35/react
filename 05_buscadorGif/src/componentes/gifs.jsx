8f8ee48a8d8ed8a68851d131765b42c78aa7309e
Daniel <studiosdopplermusic@gmail.com>
2022-06-25 19:17:55 -0500

    refactor

--- 05_buscadorGif/src/componentes/gifs.jsx
+++ 05_buscadorGif/src/componentes/gifs.jsx
@@ -1,22 +1,8 @@
-
-    const getGifs = async (categoria)=>{
-        const url = `https://api.giphy.com/v1/gifs/search?api_key=BATA8D9T2GkMUePGXGwJQq1NGNFGdqJg&q=${categoria}&limit=20`; 
-        const respuesta = await fetch(url); 
-        const {data = []} = await respuesta.json()
-        const gifs = data.map( img => ({
-            id :img.id, 
-            title: img.title,
-            url: img.images.downsized_medium.url
-        }))
-        console.log(gifs);
-}
-
+import { getGifs } from "../funciones/getGifs"
 
 export const Gifs = ({categoria}) => {
 
-
     getGifs(categoria)
-
 
   return (
     <>
