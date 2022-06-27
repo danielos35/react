6f46fed7766909f99f8c8a59d47074c80894c996
Daniel <studiosdopplermusic@gmail.com>
2022-06-27 10:39:26 -0500

    intro pruebas unitarias

---
+++ 00_Notas/04_PruebasUnitarias/intro.js
@@ -0,0 +1,21 @@
+/*
+¿QUE SON LAS PRUEBAS UNITARIAS?
+- No son un perdida de tiempo.
+- Probar puede demorar el mismo tiempo que tardó en desarrollarse la aplicación. 
+
+TIPOS DE PRUEBAS
+- Pruebas Unitarias.
+-- Estas enfocadas en pequeñas funcionalidades de la aplicación (en pequeñas pruebas de la misma), (por ejemplo probar una llanta de un carro)
+
+- Pruebas de integración.
+-- Estan enfocadas en como reaccionan grandes piezas en conjunto. (Probar las llantas con el resto del carro )
+
+CARACTERISTICAS 
+- Faciles de escribir.
+- Faciles de leer. 
+- Confiables. 
+- Rapidas. 
+- Principalmente unitarias. 
+
+
+*/ 
