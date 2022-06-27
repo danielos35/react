8f5d70d7cc02a079bad61f3e1c9fa4810c32e870
Daniel <studiosdopplermusic@gmail.com>
2022-06-27 11:00:39 -0500

    primeras pruebas

---
+++ 00_Notas/04_PruebasUnitarias/02_configuracion.js
@@ -0,0 +1,13 @@
+/*
+CONFIGURACIÓN DE VITE CON JEST.
+- Jest es un framework utilizado para la creación de pruebas unitarios con JS: https://jestjs.io/
+
+INSTALACIÓN 
+- npm install --save-dev jest
+
+CREAR SRIPT DE TEST
+- Dentro del package.json configurar el script, para que funcione como nodemon podemos usar el "jest --watchAll"
+
+CREAR OTRO DIRECTORIO
+- Directorio llamado test, que funcionar como una copia del src
+*/ 
