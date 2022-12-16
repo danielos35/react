5e87f83e9c2e0c55eb1c8dd255a42656e2384413
Daniel <studiosdopplermusic@gmail.com>
2022-12-15 19:05:00 -0500

    variables

--- intro/intro/src/FirstApp.jsx
+++ intro/intro/src/FirstApp.jsx
@@ -1,7 +1,32 @@
+/**
+ * Funcionalidades preferiblemente fuera del funcionalcomponent
+ *
+ *
+ * */
+
+const datos = {
+  nombre: "Daniel",
+  apellido: "Felipe",
+};
+
+const unNombre = function () {
+  return "Hola este es mi nombre";
+};
+
 export const App = function () {
   return (
     <>
       <h1>Hola Mundo</h1>
+
+      {/* EXPRESIONES */}
+
+      <h1>{2 + 2}</h1>
+      <hr></hr>
+      <h1>{2 + 2}</h1>
+      <h1>{() => {}}</h1>
+      <h1>{datos.nombre}</h1>
+      <h1>{datos.apellido}</h1>
+      <h1>{unNombre()}</h1>
     </>
   );
 };
---
+++ notes/fundamentos/01_variables.js
@@ -0,0 +1,10 @@
+/**
+ *
+ * Para usar variables usamos {} dentro de las etiquetas
+ * let variable = 'Daniel'
+ * <h1>{variable}</h1>
+ *
+ *
+ * NO PODEMOS USAR OBJETOS
+ *
+ */
