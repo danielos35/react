8cd640dc571f51b02d8155d0f1f59e3d267bb472
Daniel <studiosdopplermusic@gmail.com>
2022-12-24 12:54:26 -0500

    useEffect

--- intro/01_glosario/src/01_app_intro/components/GifGrid.jsx
+++ intro/01_glosario/src/01_app_intro/components/GifGrid.jsx
@@ -1,6 +1,19 @@
+// El use effect se dipara cada vez que tengamos un cambio en especifico
+import { useEffect } from "react";
 import { getGifs } from "../helpers/get_images";
 export const GifGrid = ({ categoria }) => {
-  getGifs(categoria);
+  /**
+   * ARGUMENTO 1
+   * - Lo que vayamos a ejecutar
+   *
+   * ARGUMENTO 2
+   * 	- Lo que tiene que cambiar para que se ejecute el primer argumento
+   *	- Si el arreglo de dependecias se deja vacio, el callback solo ejectará la primera vez que se renderice el componente
+   * */
+
+  useEffect(() => {
+    getGifs(categoria);
+  }, []);
   return (
     <>
       <h3>{categoria}</h3>
