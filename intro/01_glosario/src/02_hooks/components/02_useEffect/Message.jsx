71ab92652dd48a5fa17e06a47acabb1d443f10e8
Daniel <studiosdopplermusic@gmail.com>
2022-12-29 14:24:48 -0500

    useEffect precauciones

--- intro/01_glosario/src/02_hooks/components/02_useEffect/Message.jsx
+++ intro/01_glosario/src/02_hooks/components/02_useEffect/Message.jsx
@@ -1,6 +1,23 @@
 import { useEffect } from "react";
 
+/**
+ * SIEMPRE SE DEBE DE DESUCRIBIR O CERRAR VALORES QUE CAMBIEN EL ESTADO EN UN COMPONENTE FUTURO
+ *
+ *
+ * */
+
 export const Message = () => {
+  useEffect(() => {
+    const listenMouse = ({ x, y }) => {
+      const coords = { x, y };
+      console.log(coords);
+    };
+    window.addEventListener("mousemove", listenMouse);
+
+    return () => {
+      window.removeEventListener("mousemove", listenMouse);
+    };
+  });
   useEffect(() => {
     console.log("Mensaje montado");
     return () => {
