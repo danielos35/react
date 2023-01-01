abbfb3d1a86a43a04c0a98ca712f5bfce4249c05
Daniel <studiosdopplermusic@gmail.com>
2023-01-01 17:03:22 -0500

    reduce notes

---
+++ clases/01_glosario/src/03_reducer/00_notas.js
@@ -0,0 +1,18 @@
+/**
+ * REDUCER
+ *
+ * - Es una función comun y corriente que NO puede ser asincronta
+ *
+ * - Debe ser una función pura
+ * -- Debe de poder resolver todo lo que se le ingrese sin necesidad de llamar otras funciones
+ * -- No debe de realizar tareas asincronas.
+ * -- Debe de retornar siempre un estado nuevo.
+ * -- No debe de llamar el localStorage o sessionStorage
+ * -- NO debe de requerir mas que una acción que puede tener un argumento
+ *
+ *
+ * - Debe retornar siempre un nuevo estado
+ * - Usualmente recibe dos argumento (El estado inicial y la acción que quiero ejecutar)
+ * */
+
+const todoReducer = () => {};
