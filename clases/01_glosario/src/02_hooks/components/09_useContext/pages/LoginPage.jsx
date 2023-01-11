e085d906c73adcd929fb2e2d5643bdcf7f6de30d
Daniel <studiosdopplermusic@gmail.com>
2023-01-10 19:55:07 -0500

    use context

--- clases/01_glosario/src/02_hooks/components/09_useContext/pages/LoginPage.jsx
+++ clases/01_glosario/src/02_hooks/components/09_useContext/pages/LoginPage.jsx
@@ -1,5 +1,21 @@
+import { useContext } from "react"
+import { json } from "react-router-dom";
+import { UserContext } from "./context/UserContext";
+
 export const LoginPage = () => {
+
+  /**
+   * En caso de que yengamos dos contextos con el mismo nombre, suministraría el mas cercano
+   * Se debe suministrar el contexto de donde proviene la data 
+   * 
+   * */
+
+  const { user, setUser_context, clean_context } = useContext(UserContext);
   return (
-    <div>LoginPage</div>
+    <div>
+      <p>{JSON.stringify(user, null, 3)}</p>
+      <button className="btn btn-primary" onClick={() => setUser_context()}>Traer usuario</button>
+      <button className="btn btn-danger" onClick={() => clean_context()}>Limpiar usuario</button>
+    </div>
   )
 }
--- clases/01_glosario/src/02_hooks/components/09_useContext/pages/context/UserProvider.jsx
+++ clases/01_glosario/src/02_hooks/components/09_useContext/pages/context/UserProvider.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import { UserContext } from "./UserContext";
-const user = {
+const user_data = {
     id: 12,
     name: 'Daniel',
     phone: 12324
 }
 
+
+
 export const UserProvider = ({ children }) => {
+    const [user, setUser] = useState();
+    const setUser_context = () => {
+        setUser(user_data)
+    }
+
+    const clean_context = () => {
+        setUser('')
+    }
     return (
-        <UserContext.Provider value={{ hola: 'Holamundo', user }}>
+        // <UserContext.Provider value={{ hola: 'Holamundo', user }}>
+        <UserContext.Provider value={{ user, setUser_context, clean_context }}>
             {children}
         </UserContext.Provider>
     )
