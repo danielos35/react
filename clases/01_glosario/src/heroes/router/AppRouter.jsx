1e1812ce0e27df4541fccd299326c1e71a28fcf5
Daniel <studiosdopplermusic@gmail.com>
2023-01-11 22:28:40 -0500

    Navbar

--- clases/01_glosario/src/heroes/router/AppRouter.jsx
+++ clases/01_glosario/src/heroes/router/AppRouter.jsx
@@ -1,10 +1,12 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 import { LoginPage } from "../auth/pages"
 import { DcPage, MarvelPage } from "../heroes/pages"
+import { Navbar } from "../ui/components"
 
 export const AppRouter = () => {
     return (
         <>
+            <Navbar />        
             <Routes>
                 <Route path="/marvel" element={<MarvelPage />}></Route>
                 <Route path="/dc" element={<DcPage />}></Route>
