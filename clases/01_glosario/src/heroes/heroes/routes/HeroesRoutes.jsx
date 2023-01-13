08bfb4cc69cbecc944448fe22b905aae6c370944
Daniel <studiosdopplermusic@gmail.com>
2023-01-12 19:21:33 -0500

    search | hero

---
+++ clases/01_glosario/src/heroes/heroes/routes/HeroesRoutes.jsx
@@ -0,0 +1,19 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { Navbar } from "../../ui/components"
+import { DcPage, Hero, MarvelPage, Search } from "../pages"
+
+export const HeroesRoutes = () => {
+    return (
+        <>
+            <Navbar />
+            <Routes>
+                <Route path="/marvel" element={<MarvelPage />}></Route>
+                <Route path="/dc" element={<DcPage />}></Route>
+                <Route path="/search" element={<Search />}></Route>
+                <Route path="/hero" element={<Hero />}></Route>
+
+                <Route path="/" element={<Navigate to="/marvel" />}></Route>
+            </Routes>
+        </>
+    )
+}
--- clases/01_glosario/src/heroes/router/AppRouter.jsx
+++ clases/01_glosario/src/heroes/router/AppRouter.jsx
@@ -1,17 +1,15 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 import { LoginPage } from "../auth/pages"
 import { DcPage, MarvelPage } from "../heroes/pages"
+import { HeroesRoutes } from "../heroes/routes/HeroesRoutes"
 import { Navbar } from "../ui/components"
 
 export const AppRouter = () => {
     return (
-        <>
-            <Navbar />        
+        <>    
             <Routes>
-                <Route path="/marvel" element={<MarvelPage />}></Route>
-                <Route path="/dc" element={<DcPage />}></Route>
                 <Route path="login" element={<LoginPage />}></Route>
-                <Route path="/" element={<Navigate to="/marvel" />}></Route>
+                <Route path="/*" element={<HeroesRoutes />}></Route>
             </Routes>
         </>
     )
